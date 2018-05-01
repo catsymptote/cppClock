677ef2525f3f1b8e22b8cab8bbd7fad74da4a071
catsymptote <catsymptote@gmail.com>
2018-05-01 22:26:29 +0200

    Sets time. Not yet properly functional.

--- alarmClock/Clock.cpp
+++ alarmClock/Clock.cpp
@@ -2,6 +2,7 @@
 #include "Clock.h"
 
 #include <iostream>		// std::cout etc.
+//#include <string>		// string for output
 #include <stdlib.h>		// Clearing console screen
 #include <thread>		// For threading
 #include <chrono>		// Get time
@@ -12,21 +13,41 @@ using namespace std::chrono;
 
 Clock::Clock()
 {	
-	this->ms = 0;
-	this->displayClock = true;
 	this->second = 0;
 	this->minute = 0;
 	this->hour = 0;
 	this->day = 0;
-	this->week = 0;
-	this->month = 0;
 	this->year = 0;
 
-	//std::thread clkMain(clock::clockLoop);
-	Clock::clockLoop();
+	this->setTimeNow();
+	this->clockLoop();
+}
+
+Clock::Clock(unsigned int year, unsigned int day, unsigned int hour, unsigned int minute, unsigned int second)
+{
+	this->year = year;
+	this->day = day;
+	this->hour = hour;
+	this->minute = minute;
+	this->second = second;
+	
+	this->clockLoop();
 }
 
 Clock::~Clock() {}
+
+
+unsigned int Clock::getSecond() { return this->second; }
+unsigned int Clock::getMinute() { return this->minute; }
+unsigned int Clock::getHour() { return this->hour; }
+unsigned int Clock::getDay() { return this->day; }
+unsigned int Clock::getYear() { return this->year; }
+
+void Clock::setSecond(unsigned int second) { if (second < 60) this->second = second; }
+void Clock::setMinute(unsigned int minute){ if (minute < 60) this->minute = minute; }
+void Clock::setHour(unsigned int hour) { if (hour < 60) this->hour = hour; }
+void Clock::setDay(unsigned int day) { if (day < 60) this->day = day; }
+void Clock::setYear(unsigned int year) { if (year< 60) this->year = year; }
 
 
 unsigned int Clock::getTime()
@@ -41,7 +62,7 @@ unsigned int Clock::getTime()
 bool Clock::updateNeeded()
 {
 	
-	if (Clock::getTime() < this->ms + 1000)
+	if (this->getTime() < this->ms + 1000)
 		return false;
 	//std::cout << ".";
 	return true;
@@ -49,15 +70,18 @@ bool Clock::updateNeeded()
 
 void Clock::clockLoop()
 {
+	this->ms = 0;
+	this->displayClock = true;
+
 	while (true)
 	{
-		if (Clock::updateNeeded())
+		if (this->updateNeeded())
 		{
-			this->ms = Clock::getTime();
-			Clock::updateClock();
-			if (Clock::displayClock)
+			this->ms = this->getTime();
+			this->updateClock();
+			if (this->displayClock)
 			{
-				Clock::displayTime();
+				this->displayTime();
 			}
 		}
 
@@ -80,15 +104,85 @@ void Clock::displayTime()
 		return;
 	*/
 
+	/// Formatting
+	std::string time;
+	
+
 	std::cout << std::flush;
 	system("CLS");
-	std::cout << "year" << "\t: " << "day" << "\t: " << "hour" << "\t: " << "min" << "\t: " << "sec" << std::endl;
-	std::cout << year << "\t: " << day << "\t: " << hour << "\t: " << minute << "\t: " << second << std::endl;
-	//std::cout << this->ms << std::endl;
+	std::cout << "yyyy" << ":" << "ddd" << " - " << "hh" << ":" << "mm" << ":" << "ss" << std::endl;
+	//std::cout << year << "\t:" << day << "\t:" << hour << "\t:" << minute << "\t:" << second << std::endl;
+	std::cout << this->formatTime() << std::endl;
+	std::cout << this->getMonth() << std::endl;
+	
+	//std::cout << system_clock::now() << std::endl;
+}
+
+std::string Clock::formatTime()
+{
+	std::string time = "";
+
+	/// Year
+	if (this->year < 1000)
+	{
+		if (this->year < 100)
+		{
+			if (this->year < 10)
+			{
+				time += "0";
+			}
+			time += "0";
+		}
+		time += "0";
+	}
+
+	time += std::to_string(this->year);
+	time += ":";
+
+	/// Day
+	if (this->day < 100)
+	{
+		if (this->year < 10)
+		{
+			time += "0";
+		}
+		time += "0";
+	}
+
+	time += std::to_string(this->day);
+	time += " - ";
+
+	/// Hour
+	if (this->hour < 10)
+	{
+		time += "0";
+	}
+
+	time += std::to_string(this->hour);
+	time += ":";
+
+	/// Minute
+	if (this->minute< 10)
+	{
+		time += "0";
+	}
+
+	time += std::to_string(this->minute);
+	time += ":";
+	
+	/// Second
+	if (this->second < 10)
+	{
+		time += "0";
+	}
+
+	time += std::to_string(this->second);
+
+	return time;
 }
 
 
-/// Updates the numbers. Ran once every second.
+/// Updates the numbers. Adds 1 second to time.
 void Clock::updateClock()
 {
 	/// Seconds
@@ -101,10 +195,18 @@ void Clock::updateClock()
 			if (this->hour >= 24 - 1)
 			{
 				/// Days
-				if (this->day >= 365 - 1)
+				unsigned int daysInYear = 365;
+				if(this->leapYear)
+					daysInYear = 366;
+
+				if (this->day >= daysInYear - 1)
 				{
 					/// Years
 					year++;
+					if (this->isLeapYear())
+						leapYear = true;
+					else
+						leapYear = false;
 					day = 0;
 					return;
 				}
@@ -122,4 +224,101 @@ void Clock::updateClock()
 	}
 	second++;
 	return;
+}
+
+unsigned int Clock::getMonth()
+{
+	unsigned int d = this->day;
+
+	/// January
+	if (d <= 31)
+		return 1;
+	d -= 31;
+
+	/// February
+	unsigned int daysInFeb = 28;
+	if (this->leapYear)
+		daysInFeb = 29;
+	if (d <= daysInFeb)
+		return 2;
+	d -= daysInFeb;
+	
+	/// March
+	if (d <= 31)
+		return 3;
+	d -= 31;
+
+	/// April
+	if (d <= 30)
+		return 4;
+	d -= 30;
+
+	/// May
+	if (d <= 31)
+		return 5;
+	d -= 31;
+
+	/// June
+	if (d <= 30)
+		return 6;
+	d -= 30;
+
+	/// July
+	if (d <= 31)
+		return 7;
+	d -= 31;
+
+	/// August
+	if (d <= 31)
+		return 8;
+	d -= 31;
+
+	/// September
+	if (d <= 30)
+		return 9;
+	d -= 30;
+
+	/// October
+	if (d <= 31)
+		return 10;
+	d -= 31;
+
+	/// November
+	if (d <= 30)
+		return 11;
+	d -= 30;
+
+	/// December
+	if (d <= 31)
+		return 12;
+	
+	std::cout << "Month error" << std::endl;
+	return 0;
+}
+
+
+/// Add 1 second to the time per 1000 ms.
+void Clock::setTimeNow()
+{
+	unsigned int now = this->getTime() / 1000;
+	while (now > 0)
+	{
+		this->updateClock();
+		now--;
+	}
+}
+
+bool Clock::isLeapYear()
+{
+	unsigned int y = this->year;
+	bool isLeap = false;
+
+	if (y % 4 == 0)
+		isLeap = true;
+	if (y % 100)
+		isLeap = false;
+	if (y % 400)
+		isLeap = true;
+
+	return isLeap;
 }
--- alarmClock/Clock.h
+++ alarmClock/Clock.h
@@ -1,13 +1,18 @@
 #pragma once
+
+#include <string>		// string for output
+
 class Clock
 {
 public:
 	Clock();
+	Clock(unsigned int year, unsigned int day, unsigned int hour, unsigned int minute, unsigned int second);
 	~Clock();
 
 private:
 	unsigned int ms;
 	bool displayClock;
+	bool leapYear;
 
 	unsigned int second;
 	unsigned int minute;
@@ -22,9 +27,25 @@ private:
 	void updateClock();
 	void clockLoop();
 	void displayTime();
+	std::string formatTime();
+	void setTimeNow();
+	bool isLeapYear();
 
 public:
 	unsigned int getTime();
+	unsigned int getMonth();
+
+	unsigned int getSecond();
+	unsigned int getMinute();
+	unsigned int getHour();
+	unsigned int getDay();
+	unsigned int getYear();
+
+	void setSecond(unsigned int second);
+	void setMinute(unsigned int minute);
+	void setHour(unsigned int hour);
+	void setDay(unsigned int day);
+	void setYear(unsigned int year);
 
 };
 
